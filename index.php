4430fcf8d5a1e7a2e13ce596a8190ed1eb2da132
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-16 21:27:05 +0100

    5-Inclure le layout de la page de sortie

--- index.php
+++ index.php
@@ -15,5 +15,5 @@ require_once 'layouts/articles/index_html.php';
 //4-recuperation du contenu du tampon de la page d'accueil
 $pageContent = ob_get_clean();
 
-//Inclure le layout de la page de sortie
+//5-Inclure le layout de la page de sortie
 require_once 'layouts/layout_html.php';
